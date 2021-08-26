91f296e748809ae5bcc8b39e70a3c289aab7d463
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-26 23:47:41 +0300

    add ssetOpen

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,19 +1,30 @@
-import { makeStyles } from '@material-ui/core';
+import { Container, makeStyles, Modal } from '@material-ui/core';
 import { Fab, Tooltip } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import { useState } from 'react';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: 'fixed',
+    bottom: 20,
+    right: 20,
+  },
+}));
 
 const Add = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
 
   return (
     <div>
-      <Tooltip title="Add" aria-label="add">
-        <Fab color="secondary" className={classes.absolute}>
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
+        <Fab color="primary" className={classes.fab}>
           <AddIcon />
         </Fab>
       </Tooltip>
+      <Modal open={open}>
+        <Container>My modal</Container>
+      </Modal>
     </div>
   );
 };
