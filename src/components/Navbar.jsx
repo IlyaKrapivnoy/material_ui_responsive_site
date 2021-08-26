1763b5a1787e9283e049990601039ab2c4086c71
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-26 16:43:41 +0300

    add search to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,21 @@
-import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  InputBase,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  search: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   logoLg: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
@@ -20,13 +35,18 @@ function Navbar() {
 
   return (
     <AppBar>
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.logoLg}>
           Bart Dev
         </Typography>
         <Typography variant="h6" className={classes.logoSm}>
           BART
         </Typography>
+        <div className={classes.search}>
+          <SearchIcon />
+          <InputBase placeholder="Search..." />
+        </div>
+        icons
       </Toolbar>
     </AppBar>
   );
