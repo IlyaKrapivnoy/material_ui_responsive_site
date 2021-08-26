4ea76e3220d97ad363b8fa6654ffe8d44b364cb0
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-27 00:40:42 +0300

    add AvatarGroup

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,5 @@
-import { Container, makeStyles } from '@material-ui/core';
+import { Avatar, Container, makeStyles } from '@material-ui/core';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -11,7 +12,29 @@ function Rightbar() {
 
   return (
     <Container className={classes.container}>
-      <div>rightbar</div>
+      <div>Online Friends</div>
+      <AvatarGroup max={4}>
+        <Avatar
+          alt="Remy Sharp"
+          src="https://material-ui.com/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          alt="Travis Howard"
+          src="https://material-ui.com/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          alt="Cindy Baker"
+          src="https://material-ui.com/static/images/avatar/3.jpg"
+        />
+        <Avatar
+          alt="Agnes Walker"
+          src="https://material-ui.com/static/images/avatar/4.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/5.jpg"
+        />
+      </AvatarGroup>
     </Container>
   );
 }
