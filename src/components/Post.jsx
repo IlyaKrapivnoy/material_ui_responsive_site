217f4e46d4c3e176f65610e028e8356984b4cb6f
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-26 20:01:07 +0300

    add cards

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,44 @@
-import { makeStyles } from '@material-ui/core';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+  },
+  media: {
+    height: 250,
   },
 }));
 
 function Post() {
   const classes = useStyles();
 
-  return <>Post</>;
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg"
+          square
+          title="My post"
+        ></CardMedia>
+        <CardContent>
+          <Typography variant="h5">My first post</Typography>
+          <Typography variant="body">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum nulla
+            voluptates maiores! Reiciendis deleniti exercitationem maxime,
+            adipisci nam rerum enim.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
 
 export default Post;
