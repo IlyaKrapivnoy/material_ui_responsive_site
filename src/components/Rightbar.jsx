e6fa4c942d9b9f9a657f6a391def75611fcf9e72
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-27 00:46:45 +0300

    AvatarGroup styling

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,9 +1,14 @@
-import { Avatar, Container, makeStyles } from '@material-ui/core';
+import { Avatar, Container, makeStyles, Typography } from '@material-ui/core';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: '#555',
   },
 }));
 
@@ -12,8 +17,10 @@ function Rightbar() {
 
   return (
     <Container className={classes.container}>
-      <div>Online Friends</div>
-      <AvatarGroup max={4}>
+      <Typography className={classes.title} gutterBottom>
+        Online Friends
+      </Typography>
+      <AvatarGroup max={6}>
         <Avatar
           alt="Remy Sharp"
           src="https://material-ui.com/static/images/avatar/1.jpg"
@@ -22,17 +29,22 @@ function Rightbar() {
           alt="Travis Howard"
           src="https://material-ui.com/static/images/avatar/2.jpg"
         />
-        <Avatar
-          alt="Cindy Baker"
-          src="https://material-ui.com/static/images/avatar/3.jpg"
-        />
+        <Avatar alt="Cindy Baker" src="" />
         <Avatar
           alt="Agnes Walker"
           src="https://material-ui.com/static/images/avatar/4.jpg"
         />
         <Avatar
           alt="Trevor Henderson"
-          src="https://material-ui.com/static/images/avatar/5.jpg"
+          src="https://material-ui.com/static/images/avatar/6.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/7.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/8.jpg"
         />
       </AvatarGroup>
     </Container>
